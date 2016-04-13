f5755e05e0e16e79a597de2a3ed3d0cd1b1d1ab3
rtuznik <rickytuznik@live.com>
2016-04-13 16:26:06 -0700

    Added GameScoreboard GUI and other minor adjustments

--- T_Strife/src/T_Strife/GameScoreboard.java
+++ T_Strife/src/T_Strife/GameScoreboard.java
@@ -1,15 +1,32 @@
 package T_Strife;
 import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import T_Strife.Scoreboard.Score;
+
 public class GameScoreboard extends Scoreboard
 {
-	int numPlayers;
-	ArrayList<Player> players;
+	private int numPlayers;
+	private JPanel scorePanel;
+	//ArrayList<Player> players;
 	
-	public GameScoreboard(int numPlayers, ArrayList<Player> players)
+	 //Score Panel should be GridLayout(numPlayers + 1,3)
+	public GameScoreboard(ArrayList<Player> players, JPanel scorePanel)
 	{
-		this.numPlayers = numPlayers;
-		this.players = players;
+		this.numPlayers = players.size();
+		this.scorePanel = scorePanel;
+		scores = new ArrayList<Score>();
+		for(Player player : players)
+		{
+			//Only the first two values matter
+			Score currentScore = new Score(player.getScore(), player.getName(), false, 0);
+			scores.add(currentScore);
+		}
+		updateScoreBoard();
 	}
 	
 	public void updateScores(ArrayList<Player> players)
@@ -22,10 +39,50 @@ public class GameScoreboard extends Scoreboard
 					score.Points = player.getScore();
 				}
 		}
+		updateScoreBoard();
 	}
 	
-	public void displayScoreboard()
+	//Pass in the JPanel where the score will be
+	public void updateScoreBoard()
 	{
+		JLabel rLabel = new JLabel("Rank");
+		JLabel nLabel = new JLabel("Name");
+		JLabel pLabel = new JLabel("Points");
 		
+		//Center each label
+		rLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		nLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		pLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		//Add borders
+		rLabel.setBorder(BorderFactory.createEtchedBorder());
+		nLabel.setBorder(BorderFactory.createEtchedBorder());
+		pLabel.setBorder(BorderFactory.createEtchedBorder());
+
+		scorePanel.add(rLabel);
+		scorePanel.add(nLabel);
+		scorePanel.add(pLabel);
+
+		for (int index = 0; index < scores.size(); index++) {
+			Score currentScore = scores.get(index);
+			JLabel rankLabel = new JLabel(index + 1 + ".");
+			JLabel nameLabel = new JLabel(currentScore.Name);
+			JLabel pointsLabel = new JLabel(currentScore.Points + "");
+
+			//Center each label
+			rankLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			pointsLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+			//Add borders
+			rankLabel.setBorder(BorderFactory.createEtchedBorder());
+			nameLabel.setBorder(BorderFactory.createEtchedBorder());
+			pointsLabel.setBorder(BorderFactory.createEtchedBorder());
+
+			//Add to scorePanel
+			scorePanel.add(rankLabel);
+			scorePanel.add(nameLabel);
+			scorePanel.add(pointsLabel);
+		}
 	}
 }
