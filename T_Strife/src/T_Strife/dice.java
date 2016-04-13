a9643d566b2d1b31fe789b0a4b38c7b9e637ebc4
SimonK <smkim6@asu.edu>
2016-04-13 15:31:49 -0700

    fixed minor syntax errors 4-13-2016

--- T_Strife/src/T_Strife/dice.java
+++ T_Strife/src/T_Strife/dice.java
@@ -1,23 +1,24 @@
 package T_Strife;
 
-import java.util.Scanner;
+import java.util.*;
 import com.sun.xml.internal.bind.v2.runtime.Name;
 
 public class dice{
 	
 	
 	//declaring variables
-	private int die1;
-	private int die2;
+	public int die1;
+	public int die2;
 	
 	//Default Constructor
 	public dice()
 	{
-		roll(null);
+		NormalRoll(null);
+		DisRoll(null);
 	}
 	
 	
-	void roll(Player player)
+	void NormalRoll(NormalPlayer player)
 	{	
 		if(player.getType().equals("Normal"))
 		{
@@ -25,97 +26,91 @@ public class dice{
 			die2 = (int)(Math.random()*5 + 1);
 		}
 		
-		else if(player.getType().equals("Disadvantaged"))
+	}	
+	
+	void DisRoll(DisadvantagedPlayer player)
+	{
+		if(player.getType().equals("Disadvantaged"))
 		{
+			//chances needs to be modified later
 			die1 = (int)(Math.random()*10 + 1);
 			die2 = (int)(Math.random()*5 + 1);
 		}
-	}	
+	}
 	
-	public int getDie1()
-	{		
+	public int getDie1(Player player)
+	{
+		player.updateScore(player.getScore() + die1);
+		
 		return die1;
 	}
 	
-	public int getDie2()
-	{
-		return die2;
-	}
-	
-	/*
-	public int getDie2(Player player, ArrayList<Player> players)
+	public int getDie2(Player mainPlayer, ArrayList<Player> players)
 	{
 		
 		Scanner Scan = new Scanner(System.in);
 		
 		if(die2 == 1)
-		{
+		{	
+			
+			
 			System.out.println("You rolled Split!");
 			System.out.println("Choose a player you wanna split points with: ");
 			
-			String name = Scan.next();
+			String playerName = Scan.next();
 			
-			//temporary logic for testing purposes. Pop-up needs to be coded later for this part
-			while(0)
-			{	
-				
+			
 				for(Player player : players)
 				{
-					if(player.getName().equals(name))
+					if(player.getName().equals(playerName))
 						{
-							System.out.println("Splitting Points with" + name)
-							player.updateScore((int) ((player.getScore() + player.getName().getScore())/2));
-							player.getName().updateScore((int) ((player.getScore() + player.getName().getScore())/2));
+							System.out.println("Splitting Points with" + playerName);
+							mainPlayer.updateScore((int) ((mainPlayer.getScore() + player.getScore())/2));
+							player.updateScore((int) ((mainPlayer.getScore() + player.getScore())/2));
 						
-						break;
+							break;
 						}
-						else if(player.getName() != name)
+						else if(player.getName() != playerName)
 						{
-							System.out.println("Name does not Exist try again")
+							System.out.println("Name does not Exist try again");
 						}
 				}
-			}
+		}
 		
 		if(die2 == 2)
 		{
 			System.out.println("You rolled Steal!");
 			System.out.println("Choose a player to steal from: ");
 			
-			String name = Scan.next();
-			
-			//temporary logic for testing purposes. Pop-up needs to be coded later for this part
-			while(0)
-			{	
+			String playerName = Scan.next();
 				
 				for(Player player : players)
 				{
-					if(player.getName().equals(name))
+					if(player.getName().equals(playerName))
 						{
-							System.out.println("Stealing points from" + name)
-							player.updateScore((int) (player.getScore() + player.getName().getScore()));
-							player.getName().updateScore((int) 0);
+							System.out.println("Stealing points from" + playerName);
+							player.updateScore((int) (mainPlayer.getScore() + player.getScore()));
+							player.updateScore((int) 0);
 							
 							break;
 						}
-						else if(player.getName() != name)
+						else if(player.getName() != playerName)
 						{
-							System.out.println("Name does not Exist try again")
+							System.out.println("Name does not Exist try again");
 						}
 				}
 			}
 		
-		}
-		
 		if(die2 == 3)
 		{
 			System.out.println("You rolled multiply!");
-			player.updateScore((int) (player.getScore() *(1.5))) ;
+			mainPlayer.updateScore((int) (mainPlayer.getScore() *(1.5))) ;
 		}
 		
 		if(die2 == 4)
 		{
 			System.out.println("You rolled Lose Points");
-			player.updateScore(player.getScore() - die1);
+			mainPlayer.updateScore(mainPlayer.getScore() - die1);
 		}
 		
 		if(die2 == 5)
@@ -126,6 +121,4 @@ public class dice{
 		return 0;
 	}
 	
-}*/
 }
-	
