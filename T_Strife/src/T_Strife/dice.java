d82ca3d1c35a6156a9081086426f14ab302766a1
SimonK <smkim6@asu.edu>
2016-04-16 15:14:11 -0700

    modified chance for the disadvantaged player

--- T_Strife/src/T_Strife/dice.java
+++ T_Strife/src/T_Strife/dice.java
@@ -6,16 +6,18 @@ package T_Strife;
 public class dice
 {
 	/** Dice integer variables for the condition and number dice */
-	//declaring variables
 	private int die1;
 	private int die2;
+	private int chance1;
+	private int chance2;
 	
 	/**
 	 * Roll method to generate a random number for gameplay
 	 * @param player the player who is rolling the dice
 	 */
 	void roll(Player player)
-	{	
+	{		
+		
 		if(player.getType().equals("Normal"))
 		{
 			die1 = (int)(Math.random()*10 + 1);
@@ -24,10 +26,61 @@ public class dice
 		
 		else if(player.getType().equals("Disadvantaged"))
 		{
+			
 			die1 = (int)(Math.random()*10 + 1);
 			die2 = (int)(Math.random()*5 + 1);
-		}
-	}	
+			chance1 = (int)(Math.random()*10 + 1);
+			chance2 = (int)(Math.random()*10 + 1);
+		
+			//30% chance of having 1 points less than what they originally rolled
+			if(chance1 == 1 || chance1 == 2 || chance1 == 3)
+			{	
+				die1 = die1 - 1;				
+				
+				if(die1 < 0)
+				{
+					die1 = 1;
+				}
+			}
+			
+			else if(chance1 == 4 || chance1 == 5 || chance1 == 6)
+			{
+				die1 = die1 - 2;
+				
+				if(die1 < 0)
+				{
+					die1 = 1;
+				}
+			}
+			
+			else if(chance1 == 7 || chance1 == 8 || chance1 == 9)
+			{
+				die1 = die1 - 3;
+				
+				if(die1 < 0)
+				{
+					die1 = 1;
+				}
+			}
+			//10% chance of dice not being affected negatively
+			else
+			{
+				//do nothing 
+			}
+			
+			}
+			
+			//60% chance of rolling lose
+			if(chance2 == 1 || chance2 == 2 || chance2 == 3 )
+			{
+				die2 = 4;
+			}
+			else
+			{
+				
+			}
+	}
+	
 	
 	/**
 	 * Returns the dice number
