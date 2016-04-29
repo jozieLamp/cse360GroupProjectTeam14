18f398ca5920a43e9d0bd4a93f30b4b96ae2ba85
Bnydam <bnydam@asu.edu>
2016-04-28 19:20:02 -0700

    Update Main_menu.java

--- T_Strife/src/T_Strife/Main_menu.java
+++ T_Strife/src/T_Strife/Main_menu.java
@@ -7,34 +7,27 @@
 	import java.util.Random;
 	import java.util.Scanner;
 	
-	/**
-	 * Main_menu - Class that displays the main menu. Options are to start, view leaderboard, and quit.
-	 *
-	 * @author - Ellery Leung (Pin 507)
-	 */
+	
 	public class Main_menu
 	{
 		JFrame frame;
 		JButton start, leader, exit;
 		Leaderboard board;
 		
-		/**
-		 * Main_menu - Constructor for main menu
-		 */
+		
 		public Main_menu() 
 		{
-			JFrame.setDefaultLookAndFeelDecorated(true);
+//			JFrame.setDefaultLookAndFeelDecorated(true);
 			frame = new JFrame("Tendentious Strife - Main Menu");
 			frame.setSize(400, 400);
 			
 			Leaderboard board = new Leaderboard("leaderboard.txt");
 			
-			JPanel mainPanel = new JPanel(new GridLayout(2, 1));
-			JPanel topPanel = new JPanel();
-			
+			JPanel panel = new JPanel(new BorderLayout());
 			JLabel name = new JLabel("TENDENTIOUS STRIFE");
 			name.setBounds(200, 50, 100, 20);
-			topPanel.add(name);
+			panel.add(name, BorderLayout.NORTH);
+			
 			
 			JPanel centerPanel = new JPanel(new GridLayout(3, 1));
 			
@@ -44,7 +37,6 @@
 			leader.setBounds(200, 250, 100, 20);
 			exit = new JButton("Exit");
 			exit.setBounds(200, 300, 100, 20);
-			
 			centerPanel.add(start);
 			centerPanel.add(leader);
 			centerPanel.add(exit);
@@ -53,24 +45,18 @@
 			leader.addActionListener(new ButtonListener());
 			exit.addActionListener(new ButtonListener());
 	
-			topPanel.setBounds(200, 50, 100, 20);
-			mainPanel.add(topPanel);
-			mainPanel.add(centerPanel);
-			frame.add(mainPanel);
+//			frame.add(buttons);
+			panel.setBounds(200, 50, 100, 20);
+			frame.add(panel);
+			frame.add(centerPanel);
 			frame.setVisible(true);
 		}
 		
-		/**
-		 * Main - Begins the main menu GUI
-		 */
 		public static void main(String[] args)
 		{
 			new Main_menu();
 		}
-		
-		/**
-		 * ButtonListener - Button Listener class to detect which button is pressed - Start, View Leaderboard, or Exit.
-		 */
+
 		private class ButtonListener implements ActionListener
 		{
 			/**
